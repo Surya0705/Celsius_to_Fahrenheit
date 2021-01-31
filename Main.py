15c7ac59e326baee7b3e125a65bd89bb531f46c9
Surya Thakur <77577564+Surya0705@users.noreply.github.com>
2021-01-31 12:05:43 +0530

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,3 @@
+a = int(input("Enter the temperature in Celsius: ")) # Taking the input from user.
+b = (a * 9/5) + 32 # Converting it to Fahrenheit.
+print("Temperature in Fahrenheit is", b) # Printing the Output.
